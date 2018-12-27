7df1e81766eca2b792d05774978f662955e8a7be
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-12-26 20:12:50 -0600

    stock prices

---
+++ Best_time_to_buy_and_sell_stocks.js
@@ -0,0 +1,10 @@
+function maxProfit(prices){
+    // Say you have an array for which the ith element is 
+    //the price of a given stock on day i.
+    // If you were only permitted to complete at most one
+    // transaction (i.e., buy one and sell one share of the stock),
+    // design an algorithm to find the maximum profit.
+    // Note that you cannot sell a stock before you buy one.
+}
+maxProfit([7,1,5,3,6,4])
+//output: 5
